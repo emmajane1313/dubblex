a7c812e9dbd689e54562dddad9cd064b975bfa8e
emmajane1313 <emma.jane@digitalax.xyz>
2025-02-25 20:23:20 -0500

    jawg

--- src/app/componentes/useMapa.ts
+++ src/app/componentes/useMapa.ts
@@ -12,18 +12,17 @@ const useMapa = () => {
       zoom: 14,
     });
 
-    var Stadia_AlidadeSmoothDark = L.tileLayer(
-      "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-
+    L.tileLayer(
+      `https://tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token={accessToken}`,
       {
         tileSize: 512,
         zoomOffset: -1,
         minZoom: 1,
         crossOrigin: true,
         referrerPolicy: "strict-origin-when-cross-origin",
-        accessToken: process.env.NEXT_PUBLIC_STADIUM,
+        accessToken: process.env.NEXT_PUBLIC_JAWG,
         attribution:
-          '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }
     ).addTo(map);
   }, []);
